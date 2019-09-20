24ac8fd3119129ab21885a12b0db328ebb48f2ca
Shan Shan Huang <shuang@sogyo.nl>
2019-09-20 16:39:32 +0200

    checks for horizontal + vertical win

--- app/src/main/java/nl/sogyo/connectfour/gameBoard.kt
+++ app/src/main/java/nl/sogyo/connectfour/gameBoard.kt
@@ -6,34 +6,63 @@
  */
 package nl.sogyo.connectfour
 
-class GameBoard(val width: Int, val height: Int) {
-    val NO_DISCS_CONNECTED_FOR_WIN = 4
+class GameBoard(private val width: Int, private val height: Int) {
+    private val NO_DISCS_CONNECTED_FOR_WIN = 4
     val gridContainer = List(height) {row -> (List(width) {column -> GridCell(null)})}
     val discs = MutableList(height * width) {disc -> if ( disc % 2 == 0) Disc(DiscColor.YELLOW) else Disc(DiscColor.RED)}
 
     fun isInputValid(inputColumn: Int) = inputColumn in 0 until width
-    fun isSpaceAvailableInColumnForDisc(inputColumn: Int) = gridContainer[0][inputColumn].isEmpty()
-    fun getDiscToPlay() = discs.removeAt(0)
+    private fun isSpaceAvailableInColumnForDisc(inputColumn: Int) = gridContainer[0][inputColumn].isEmpty()
+    private fun getDiscToPlay() = discs.removeAt(0)
 
-    fun getGridCellFromInput(inputColumn: Int): GridCell {
+    private fun getGridCellFromInput(inputColumn: Int): GridCell {
         for (row in height - 1 downTo 1) {
             if (gridContainer[row][inputColumn].isEmpty()) return gridContainer[row][inputColumn]
         }
         return gridContainer[0][inputColumn]
     }
 
-    fun placeDiscInGridCell(inputColumn: Int) {
+    fun dropDiscIntoColumn(inputColumn: Int) {
         if (isInputValid(inputColumn) && isSpaceAvailableInColumnForDisc(inputColumn)) {
             getGridCellFromInput(inputColumn).placeDisc(getDiscToPlay())
         }
     }
 
-    fun isFourConnectedHorizontally(): Boolean {
+    private fun areSameDiscsInGridCells(gridCells: List<GridCell>): Boolean {
+        val distinctDiscs = gridCells.map{it.disc}.distinct()
+        return (distinctDiscs[0] != null && distinctDiscs.size == 1)
+    }
+
+    fun isConnectedHorizontally(): Boolean {
         for (row in height - 1 downTo 0) {
             for (col in 0 .. width - (NO_DISCS_CONNECTED_FOR_WIN)) {
-                val fourGridCells = gridContainer[row].slice(col until col + NO_DISCS_CONNECTED_FOR_WIN)
+                val gridCells = gridContainer[row].slice(col until col + NO_DISCS_CONNECTED_FOR_WIN)
+                if (areSameDiscsInGridCells(gridCells)) {return true}
             }
         }
-        return true
+        return false
+    }
+
+    private fun getTransposedGridContainer(): List< List<GridCell> > {
+        val transposedGridContainer = mutableListOf< MutableList<GridCell> >()
+        for (col in 0 until width) {
+            val rowInTransposedGrid = mutableListOf<GridCell>()
+            for (row in gridContainer) {
+                rowInTransposedGrid.add(row[col])
+            }
+            transposedGridContainer.add(rowInTransposedGrid)
+        }
+        return transposedGridContainer
+    }
+
+    fun isConnectedVertically(): Boolean {
+        val transposedGridContainer = getTransposedGridContainer()
+        for (row in transposedGridContainer) {
+            for (col in 0 until NO_DISCS_CONNECTED_FOR_WIN - 1) {
+                val gridCells = row.slice(col until col + NO_DISCS_CONNECTED_FOR_WIN)
+                if (areSameDiscsInGridCells(gridCells)) return true
+            }
+        }
+        return false
     }
 }
--- app/src/test/java/nl/sogyo/connectfour/ConnectFourGameTest.kt
+++ app/src/test/java/nl/sogyo/connectfour/ConnectFourGameTest.kt
@@ -35,31 +35,40 @@ class ConnectFourGameTest {
     @Test
     fun isDiscCorrectlyPlacedInGridCellTest() {
         TestCase.assertEquals(null, gameBoard!!.gridContainer[5][0].disc)
-        gameBoard!!.placeDiscInGridCell(0)
+        gameBoard!!.dropDiscIntoColumn(0)
         TestCase.assertEquals(Disc(DiscColor.YELLOW), gameBoard!!.gridContainer[5][0].disc)
         TestCase.assertEquals(41, gameBoard!!.discs.size)
 
         TestCase.assertEquals(null, gameBoard!!.gridContainer[4][0].disc)
-        gameBoard!!.placeDiscInGridCell(0)
+        gameBoard!!.dropDiscIntoColumn(0)
         TestCase.assertEquals(Disc(DiscColor.RED), gameBoard!!.gridContainer[4][0].disc)
         TestCase.assertEquals(40, gameBoard!!.discs.size)
     }
 
     @Test
     fun checkForHorizontalWinTest() {
-        gameBoard!!.placeDiscInGridCell(0)
-        gameBoard!!.placeDiscInGridCell(0)
-        gameBoard!!.placeDiscInGridCell(1)
-        gameBoard!!.placeDiscInGridCell(1)
-        gameBoard!!.placeDiscInGridCell(2)
-        gameBoard!!.placeDiscInGridCell(2)
-        gameBoard!!.placeDiscInGridCell(3)
+        gameBoard!!.dropDiscIntoColumn(0)
+        gameBoard!!.dropDiscIntoColumn(0)
+        gameBoard!!.dropDiscIntoColumn(1)
+        gameBoard!!.dropDiscIntoColumn(1)
+        gameBoard!!.dropDiscIntoColumn(2)
+        gameBoard!!.dropDiscIntoColumn(2)
+        gameBoard!!.dropDiscIntoColumn(3)
+
+        TestCase.assertTrue(gameBoard!!.isConnectedHorizontally())
+    }
+
+    @Test
+    fun checkForVerticalWinTest() {
+        gameBoard!!.dropDiscIntoColumn(0)
+        gameBoard!!.dropDiscIntoColumn(1)
+        gameBoard!!.dropDiscIntoColumn(0)
+        gameBoard!!.dropDiscIntoColumn(1)
+        gameBoard!!.dropDiscIntoColumn(0)
+        gameBoard!!.dropDiscIntoColumn(1)
+        gameBoard!!.dropDiscIntoColumn(0)
 
 
-//        for (row in gameBoard!!.gridContainer) {
-//            row.forEach{print("${it.disc} ")}
-//            println()
-//        }
-        TestCase.assertTrue(gameBoard!!.isFourConnectedHorizontally())
+        TestCase.assertTrue(gameBoard!!.isConnectedVertically())
     }
 }
