9aa29e15fa1b8c34a9f04744f95b2facbc358e75
Shan Shan Huang <shuang@sogyo.nl>
2019-09-25 16:05:55 +0200

    added playerbar and buttons to drop disc for front-end

--- app/src/main/java/nl/sogyo/connectfour/MainActivity.kt
+++ app/src/main/java/nl/sogyo/connectfour/MainActivity.kt
@@ -1,12 +1,46 @@
 package nl.sogyo.connectfour
 
+import kotlinx.android.synthetic.main.activity_main.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout;
+
 
 class MainActivity : AppCompatActivity() {
+
+    val gameBoard = GameBoard(7, 6)
+    val gridContainer = gameBoard.gridContainer
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showGridContainerView()
+
     }
+
+    private fun showGridContainerView() {
+        val param = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1f)
+
+        for (row in gridContainer) {
+            val rowsInGrid = LinearLayout(this)
+            for (gridCell in row) {
+                rowsInGrid.addView(createGridCellView(gridCell), param)
+            }
+            gridContainerLayout.addView(rowsInGrid)
+        }
+    }
+
+    private fun createGridCellView(gridCell: GridCell): ImageView {
+        val gridCellView = ImageView(this)
+
+        when {
+            gridCell.isEmpty() -> gridCellView.setImageResource(R.drawable.no_disc)
+            gridCell.disc!!.myDisc -> gridCellView.setImageResource(R.drawable.soot_disc)
+            else -> gridCellView.setImageResource(R.drawable.sootsprite)
+        }
+        return gridCellView
+    }
+
 }
